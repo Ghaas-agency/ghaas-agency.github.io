a00c36f24734ab8cc885d2e6fa0dd9e335c36784
Akshay Dhiman <akshay.virgo@gmail.com>
2018-08-03 13:30:49 +0530

    added faq page, style fixes

--- fitato-fit/path----43dd20cc64264c833f55.js
+++ fitato-fit/path----43dd20cc64264c833f55.js
@@ -1,2 +1,2 @@
-webpackJsonp([60335399758886],{110:function(t,a){t.exports={data:{site:{siteMetadata:{title:"Fitato | One Pass to Fitness"}}},layoutContext:{}}}});
+webpackJsonp([60335399758886],{111:function(t,a){t.exports={data:{site:{siteMetadata:{title:"Fitato | One Pass to Fitness"}}},layoutContext:{}}}});
 //# sourceMappingURL=path----43dd20cc64264c833f55.js.map
