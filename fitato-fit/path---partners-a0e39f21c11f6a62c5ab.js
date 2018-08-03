9b2a16c9221392163a19cbcb68f33150dd26814d
Akshay Dhiman <akshay.virgo@gmail.com>
2018-08-03 20:08:05 +0530

    responsive header, deleted menu.js, updated main menu, style fixes

--- fitato-fit/path---partners-a0e39f21c11f6a62c5ab.js
+++ fitato-fit/path---partners-a0e39f21c11f6a62c5ab.js
@@ -1,2 +1,2 @@
-webpackJsonp([0x8355993cac37],{368:function(t,c){t.exports={pathContext:{}}}});
+webpackJsonp([0x8355993cac37],{367:function(t,c){t.exports={pathContext:{}}}});
 //# sourceMappingURL=path---partners-a0e39f21c11f6a62c5ab.js.map
