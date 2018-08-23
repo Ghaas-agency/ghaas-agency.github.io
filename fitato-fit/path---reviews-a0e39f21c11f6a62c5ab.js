3f310fa9881817eede0795696c646023752e1a31
Akshay Dhiman <akshay.virgo@gmail.com>
2018-08-23 18:44:01 +0530

    Fitato-fit: added meta description, updated titles, /rakhi-offer content updates, updated /404

--- fitato-fit/path---reviews-a0e39f21c11f6a62c5ab.js
+++ fitato-fit/path---reviews-a0e39f21c11f6a62c5ab.js
@@ -1,2 +1,2 @@
-webpackJsonp([0xd739df1518b4],{387:function(t,n){t.exports={pathContext:{}}}});
+webpackJsonp([0xd739df1518b4],{395:function(t,n){t.exports={pathContext:{}}}});
 //# sourceMappingURL=path---reviews-a0e39f21c11f6a62c5ab.js.map
--- fitato-fit/path---workplace-a0e39f21c11f6a62c5ab.js
+++ fitato-fit/path---workplace-a0e39f21c11f6a62c5ab.js
@@ -1,2 +1,2 @@
-webpackJsonp([0xe86015871934],{388:function(t,e){t.exports={pathContext:{}}}});
+webpackJsonp([0xe86015871934],{396:function(t,e){t.exports={pathContext:{}}}});
 //# sourceMappingURL=path---workplace-a0e39f21c11f6a62c5ab.js.map
