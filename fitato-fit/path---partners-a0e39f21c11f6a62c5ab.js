84e9ec12ee490b56b6cbd46cbbb94c680e52e63e
Akshay Dhiman <akshay.virgo@gmail.com>
2018-08-27 12:48:32 +0530

    Fitato-fit: update navbar design, tracking codes, subdomain pages removal, withPrefix fixes, design updates

--- fitato-fit/path---partners-a0e39f21c11f6a62c5ab.js
+++ fitato-fit/path---partners-a0e39f21c11f6a62c5ab.js
@@ -1,2 +1,2 @@
-webpackJsonp([0x8355993cac37],{391:function(t,c){t.exports={pathContext:{}}}});
+webpackJsonp([0x8355993cac37],{377:function(t,c){t.exports={pathContext:{}}}});
 //# sourceMappingURL=path---partners-a0e39f21c11f6a62c5ab.js.map
