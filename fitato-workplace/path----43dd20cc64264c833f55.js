d6f5afcaa8da039f5650b1582e6ba916667bd18b
Akshay Dhiman <akshay.virgo@gmail.com>
2018-09-03 12:22:59 +0530

    Fitato-workplace: added form to the last section, form emails, style updates

--- fitato-workplace/path----43dd20cc64264c833f55.js
+++ fitato-workplace/path----43dd20cc64264c833f55.js
@@ -1,2 +1,2 @@
-webpackJsonp([60335399758886],{106:function(t,a){t.exports={data:{site:{siteMetadata:{title:"Fitato | One Pass to Fitness"}}},layoutContext:{}}}});
+webpackJsonp([60335399758886],{107:function(t,a){t.exports={data:{site:{siteMetadata:{title:"Fitato | One Pass to Fitness"}}},layoutContext:{}}}});
 //# sourceMappingURL=path----43dd20cc64264c833f55.js.map
