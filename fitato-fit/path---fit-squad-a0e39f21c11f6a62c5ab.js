87f448d3536646e5d193c50f1084f67fd2ea4e3e
Akshay Dhiman <akshay.virgo@gmail.com>
2018-09-06 21:30:59 +0530

    fitato-fit: created /thank-you, /fit-sqaud, contact forms replaced with mailchimp, content style updates

---
+++ fitato-fit/path---fit-squad-a0e39f21c11f6a62c5ab.js
@@ -0,0 +1,2 @@
+webpackJsonp([0xb320b0d2b2af],{384:function(t,b){t.exports={pathContext:{}}}});
+//# sourceMappingURL=path---fit-squad-a0e39f21c11f6a62c5ab.js.map
--- fitato-fit/path---purchase-fitato-a0e39f21c11f6a62c5ab.js
+++ fitato-fit/path---purchase-fitato-a0e39f21c11f6a62c5ab.js
@@ -1,2 +1,2 @@
-webpackJsonp([0x5e87ffdfff26],{379:function(f,t){f.exports={pathContext:{}}}});
+webpackJsonp([0x5e87ffdfff26],{388:function(f,t){f.exports={pathContext:{}}}});
 //# sourceMappingURL=path---purchase-fitato-a0e39f21c11f6a62c5ab.js.map
