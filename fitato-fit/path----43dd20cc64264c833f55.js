6e9609511ecd2f0ca13f9017379d013cf23aafc9
Akshay Dhiman <akshay.virgo@gmail.com>
2018-09-07 14:19:49 +0530

    fitato-fit: image optimization, style fixes

--- fitato-fit/path----43dd20cc64264c833f55.js
+++ fitato-fit/path----43dd20cc64264c833f55.js
@@ -1,2 +1,2 @@
-webpackJsonp([60335399758886],{115:function(t,a){t.exports={data:{site:{siteMetadata:{title:"Fitato | One Pass to Fitness"}}},layoutContext:{}}}});
+webpackJsonp([60335399758886],{116:function(t,a){t.exports={data:{site:{siteMetadata:{title:"Fitato | One Pass to Fitness"}}},layoutContext:{}}}});
 //# sourceMappingURL=path----43dd20cc64264c833f55.js.map
