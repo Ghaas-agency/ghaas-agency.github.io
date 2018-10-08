2d120418b802a083bb9c04a8d22c8af393858370
Akshay Dhiman <akshay.virgo@gmail.com>
2018-10-08 17:17:01 +0530

    fitato-hyderabad: init

---
+++ fitato-hyderabad/2-96f03e1e3febc110c5b0.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{140:function(a){a.exports={pages:[{componentChunkName:"component---src-pages-index-js",jsonName:"index",path:"/"}],dataPaths:{"dev-404-page-5f9":"920/path---dev-404-page-5-f-9-fab-NZuapzHg3X9TaN1iIixfv1W23E",index:"173/path---index-6a9-NZuapzHg3X9TaN1iIixfv1W23E","sq--src-components-index-js":3470870683}}}}]);
+//# sourceMappingURL=2-96f03e1e3febc110c5b0.js.map
