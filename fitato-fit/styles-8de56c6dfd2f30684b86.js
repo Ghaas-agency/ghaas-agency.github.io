438b45210461045e425941cb47abb1f6a1f62453
Akshay Dhiman <akshay.virgo@gmail.com>
2019-08-16 17:44:34 +0530

    fitato-fit: add offer popup

---
+++ fitato-fit/styles-8de56c6dfd2f30684b86.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{224:function(n,o,c){},240:function(n,o,c){},243:function(n,o,c){},255:function(n,o,c){},295:function(n,o,c){},296:function(n,o,c){},297:function(n,o,c){},298:function(n,o,c){}}]);
+//# sourceMappingURL=styles-8de56c6dfd2f30684b86.js.map
