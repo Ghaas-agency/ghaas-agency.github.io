bf6035d6e2fafda08d832b83d03e227ae6130614
Akshay Dhiman <akshay.virgo@gmail.com>
2019-11-19 15:41:57 +0530

    fitato-offers: add fitnov offer with countdown timer

---
+++ fitato-offers/fitnov/assets/js/timer.js
@@ -0,0 +1,18 @@
+var countDownDate = new Date("November 22, 2019 23:59:59").getTime();
+
+var x = setInterval(function() {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  var hours = Math.floor((distance / (1000 * 60 * 60)));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("time-left").innerHTML = hours + ":" + minutes + ":" + seconds + " left till the offer ends.";
+  document.getElementById('offer-data').style.display = 'block';
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("time-left").innerHTML = "Offer expired!";
+    document.getElementById('offer-data').innerHTML = '<p class="red bold" style="font-size: 2em; margin-top: 45px">Offer expired!</p>';
+  }
+}, 1000);
