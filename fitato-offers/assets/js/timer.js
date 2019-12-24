9e5846663d0a41b787e12d71380357013155bbd5
Akshay Dhiman <akshay.virgo@gmail.com>
2019-12-24 18:53:34 +0530

    fitato-offers: add offers FIT5050, FIT2020

--- fitato-offers/assets/js/timer.js
+++ fitato-offers/assets/js/timer.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("November 22, 2019 23:59:59").getTime();
+var countDownDate = new Date("December 31, 2019 23:59:59").getTime();
 
 var x = setInterval(function() {
   var now = new Date().getTime();
